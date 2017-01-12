2a81fcd6bf92fcd2024f0d57a5105e336e9ad4d2
Kamasala <SKamasala2@ad.harman.com>
2017-01-12 14:45:22 +0530

    first commit

---
+++ src/main/java/library/ons/com/onslib/Utils.java
@@ -0,0 +1,12 @@
+package library.ons.com.onslib;
+
+/**
+ * Created by SKamasala2 on 1/12/2017.
+ */
+
+public class Utils {
+
+    public static int addition(int a, int b){
+        return  a+b;
+    }
+}
